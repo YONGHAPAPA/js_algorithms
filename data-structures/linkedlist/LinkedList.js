3a34c20c635495236f45f1a9e41afff52d2f583e
YONGHAPAPA <zerobreaker@gmail.com>
2019-11-17 19:14:13 +0900

    20191117

--- data-structures/linkedlist/LinkedList.js
+++ data-structures/linkedlist/LinkedList.js
@@ -9,63 +9,70 @@ class LinkedList {
     }
 
     prepend(value){
-        const node = new LinkedNode(value);
+        const newNode = new LinkedNode(value);
 
         if(!this.head){
-            this.head = node;
-            this.tail = node;
-            return this;
-        } else {
-
-            //console.log(this.head.next === this.tail);
-            node.next = this.head; //the first node.next === this.tail
-            this.head = node;
+            this.head = newNode;
+            this.tail = newNode;
             return this;
         }
+
+        newNode.next = this.head;
+        this.head = newNode;
+        return this;
     }
 
 
     append(value){
-        //console.log(this.head === this.tail);
-        const node = new LinkedNode(value);
+
+        const newNode = new LinkedNode(value, 'X');
 
         if(!this.head){
-            this.head = node;
-            this.tail = node;
+            this.head = newNode;
+            this.tail = newNode;
             return this;
-        } 
-        
-        this.tail.next = node; //this.head.next & this.tail.next have same reference. (this this.tail.next means the first head.next)
-        this.tail = node;  //the only first head.next & this.tail.next 
+        }
+
+        var lastNode = null;
+        let currrentNode = this.head;
+        while(currrentNode){
+            if(!currrentNode.next){
+                lastNode = currrentNode;
+            }
+
+            currrentNode = currrentNode.next;
+        }
+
+        //console.log(lastNode);
+        //console.log(this.tail.next);
+        //console.log(lastNode === this.tail.next);
+
+        this.tail.next = newNode;
+
+        //console.log(this.tail.next);
+        //console.log(lastNode);
+        //console.log(this.tail.next);
+        //console.log(lastNode === this.tail.next);
+        this.tail = newNode;
+
+        console.log(lastNode);
+        console.log(this.tail.next);
+        console.log(lastNode === this.tail.next);
+
+        //console.log(lastNode === this.tail.next);
+
+        return this;       
     }
 
     reverse(){
-        let currentNode = this.head;
-        let reverse = null;
-
-        while(currentNode){
-
-            let newNode = new LinkedNode(currentNode.value);
-
-            if(!reverse){
-                reverse = newNode;
-                this.tail = newNode;
-            } else {
-                newNode.next = reverse;
-                reverse = newNode;
-            }
-
-            currentNode = currentNode.next;
-            this.head = reverse;
-        }
-
-        return this;
+       
     }
 
     toArray(){
+      
         const nodes = new Array();
-
         let currentNode = this.head;
+
         while(currentNode){
             nodes.push(currentNode.value);
             currentNode = currentNode.next;
