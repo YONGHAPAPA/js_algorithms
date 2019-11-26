132c087938e6877dfe61409d4d064eb0de90dbf1
YONGHAPAPA <zerobreaker@gmail.com>
2019-11-26 13:59:37 +0900

    20191126

--- data-structures/Linkedlist/LinkedList.js
+++ data-structures/Linkedlist/LinkedList.js
@@ -10,6 +10,7 @@ class LinkedList{
         this.tail = tail;
     }
 
+    /*
     prepend(value){
         const newNode = new LinkedNode(value);
 
@@ -94,6 +95,78 @@ class LinkedList{
 
     toString(cb){
         return this.toArray().map(item => item.toString(cb)).toString();
+    }
+    */
+
+    //2019-11-26
+    prepend(value){
+        const newNode = new LinkedNode(value);
+
+        if(!this.head){
+            this.head = newNode;
+            this.tail = newNode;
+            return this;
+        }
+
+        newNode.next = this.head;
+        this.head = newNode;
+        return this;
+    }
+
+    append(value){
+        const newNode = new LinkedNode(value);
+        const someNode = new LinkedNode(100);
+
+        if(!this.head){
+            this.head = newNode;
+            this.tail = newNode;
+            return this;
+        }
+
+        this.tail.next = newNode;
+        //this.tail = someNode;
+        this.tail = newNode;        //이것으로 this.head의 마지막 next 값, this.tail.next, this.tail 이 모두 newNode로 같은 값을 참조하게 됨
+        return this;
+    }
+
+    reverse(){
+        
+        let currNode = this.head;
+        let revNode = null;
+        let nextNode = null;
+        
+        while(currNode){
+            nextNode = currNode.next;
+            currNode.next = revNode;    //revNode가 null 였을때 this.head 가 최종 tail 과 동일해진다. 
+            revNode = currNode;
+            currNode = nextNode;
+        }
+
+
+        //console.log(this.head);
+        this.tail = this.head;
+        this.head = revNode;
+        return this;
+    }
+
+    find(value){
+
+    }
+
+    toArray(){
+        const nodes = new Array();
+        let currNode = this.head;
+
+        while(currNode){
+            nodes.push(currNode.value);
+            currNode = currNode.next;
+        }
+
+        return nodes;
+    }
+
+    toString(cb){
+
     }
 }
 
--- data-structures/linkedlist/index.js
+++ data-structures/linkedlist/index.js
@@ -1,29 +1,48 @@
+const LinkedNode = require('./LinkedNode');
 const LinkedList = require('./LinkedList');
 const list = new LinkedList();
 
 //prepend node
 for(let i=0; i>=-3; i--){
-    list.prepend(i);
+    //list.prepend(i);
 }
+
+//console.log(list.head);
+//console.log(list.tail);
 
 //append node
 for(let i=1; i<=3; i++){
-    list.append(i);
+    //list.append(i);
 }
 
-//console.log(list.toString());
-list.reverse();
+//console.log(list.toArray());
+
+//list.reverse();
+//console.log(list.toArray());
+
 
 //console.log(list.toString());
-list.reverse();
+//list.reverse();
 
 //console.log(list.toString());
 //console.log(list.compare(1,1));
 const fn = function(val){
-    console.log(val);
+    //console.log(val);
 }
-const findNode = list.find(3, fn);
-console.log(findNode);
+//const findNode = list.find(3, fn);
+//console.log(findNode);
+
+
+const a = new LinkedNode(1,2);
+//console.log(a);
+const b = a;
+const c = b;
+c.next = 9;
+
+
+console.log(a);
+console.log(b);
+console.log(c);
 
 
 
